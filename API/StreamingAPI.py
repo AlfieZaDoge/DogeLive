46eea965a0d349a04177b717bf12c5c2a2c1cb43
AlfieZaDoge <yepperyeppersone@gmail.com>
2025-07-14 18:41:35 +0100

    Update StreamingAPI.py

--- API/StreamingAPI.py
+++ API/StreamingAPI.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../config.json'))
+config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../config.json'))
 
 with open(config_path, 'r') as f:
     config = json.load(f)
